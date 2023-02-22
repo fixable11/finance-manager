9083ac05233dceedf41292708b496ea289595d5c
Mykyta Borovyk <mykyta.borovyk@danads.se>
2023-02-22 11:32:58 +0200

    Added category and transactions

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BanksModule } from './banks/banks.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { CategoryModule } from './category/category.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [BanksModule],
+  imports: [BanksModule, TransactionsModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
