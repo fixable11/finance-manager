d11af77efbd0f49735e621f3c01e23e9d1120481
Mykyta Borovyk <mykyta.borovyk@danads.se>
2023-02-22 10:47:12 +0200

    Banks CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BanksModule } from './banks/banks.module';
 
 @Module({
-  imports: [],
+  imports: [BanksModule],
   controllers: [AppController],
   providers: [AppService],
 })
